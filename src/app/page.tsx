5c48bd9fe786bdfffd5c9ef16d3a19357211a0c1
HumzahJavid <25638011+HumzahJavid@users.noreply.github.com>
2024-06-03 15:35:22 +0100

    Add text editor to page, attempt to position correctly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,30 @@
 import InstrumentList from "@/components/InstrumentList";
 import TextEditor from "@/components/TextEditor";
+import { Box } from "@mui/material";
 
-export default function Instruments() {
-  return <InstrumentList />;
+export default function specificationEditor() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "row",
+        objectFit: "contains",
+      }}
+    >
+      <InstrumentList />
+      <Box
+        sx={{
+          display: "flex-grow",
+          justifyContent: "right",
+          width: "100%",
+          maxWidth: 100,
+        }}
+      >
+        <TextEditor />
+      </Box>
+    </Box>
+  );
+}
+
 }
