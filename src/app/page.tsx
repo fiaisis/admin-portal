7bb33da5250f9acb5be63437b6bc66732eab22d2
HumzahJavid <25638011+HumzahJavid@users.noreply.github.com>
2024-07-01 10:44:47 +0100

    Restyle landing page and move buttons into top left corner
    To simplify code and make space for future buttons and links as requested

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,19 @@
-"use client";
-import { Box } from "@mui/material";
-
 import { Button, Container } from "@mui/material";
 const DEFAULT_INSTRUMENT = "MARI";
 
 export default function Home() {
   return (
-    <Container
-      style={{
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        maxWidth: "md",
-      }}
-    >
-      <Box
-        sx={{
-          display: "flex",
-          flex: 5,
-          justifyContent: "space-between",
-          width: "50%",
-        }}
+    <Container sx={{ display: "flex", gap: 1, mt: 1, mx: 0 }}>
+      <Button
+        variant="contained"
+        size="large"
+        href={`/specification/${DEFAULT_INSTRUMENT}`}
       >
-        <Button
-          variant="contained"
-          style={{ width: 300, height: 300 }}
-          href={`/specification/${DEFAULT_INSTRUMENT}`}
-        >
-          Specifications
-        </Button>
-        <Button
-          disabled
-          variant="contained"
-          style={{ width: 300, height: 300 }}
-        >
-          fileshare
-        </Button>
-      </Box>
+        Specifications
+      </Button>
+      <Button disabled variant="contained" size="large">
+        fileshare
+      </Button>
     </Container>
   );
 }
