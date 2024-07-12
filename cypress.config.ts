0dbf30138e43ca200a43dd8d0199e987841409f9
HumzahJavid <25638011+HumzahJavid@users.noreply.github.com>
2024-07-12 21:23:50 +0100

    Add basepath to cypress tests (path resolution)
    To fix tests, since pushing to staging various commits changed how the nextjs project resolves paths

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:3000/admin-portal',
     supportFile: false,
     trashAssetsBeforeRuns: true,
     setupNodeEvents(on, config) {
